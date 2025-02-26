7ff321af77b9fc0e3173ff5f4d948830009e8421
PulsarHaze <2897080442@qq.com>
2025-02-26 12:18:05 +0800

    lab3

---
+++ lab3/timingtest/TimeAList.java
@@ -0,0 +1,42 @@
+package timingtest;
+import edu.princeton.cs.algs4.Stopwatch;
+
+/**
+ * Created by hug.
+ */
+public class TimeAList {
+    private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+        System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
+        System.out.printf("------------------------------------------------------------\n");
+        for (int i = 0; i < Ns.size(); i += 1) {
+            int N = Ns.get(i);
+            double time = times.get(i);
+            int opCount = opCounts.get(i);
+            double timePerOp = time / opCount * 1e6;
+            System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
+        }
+    }
+
+    public static void main(String[] args) {
+        timeAListConstruction();
+    }
+
+    public static void timeAListConstruction() {
+        // TODO: YOUR CODE HERE
+        int[] size = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+        AList<Integer> Ns = new AList<>();
+        for (int i = 0; i < size.length; i++) {
+            Ns.addLast(size[i]);
+        }
+        AList<Double> times = new AList<>();
+        for (int i = 0; i < size.length; i++) {
+            AList<Integer> list = new AList<>();
+            Stopwatch timer = new Stopwatch();
+            for (int j = 0; j < size[i]; j++) {
+                list.addLast(j);
+            }
+            times.addLast(timer.elapsedTime());
+        }
+        printTimingTable(Ns, times, Ns);
+    }
+}
---
+++ lab3/timingtest/TimeSLList.java
@@ -0,0 +1,48 @@
+package timingtest;
+import edu.princeton.cs.algs4.Stopwatch;
+
+/**
+ * Created by hug.
+ */
+public class TimeSLList {
+    private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+        System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
+        System.out.printf("------------------------------------------------------------\n");
+        for (int i = 0; i < Ns.size(); i += 1) {
+            int N = Ns.get(i);
+            double time = times.get(i);
+            int opCount = opCounts.get(i);
+            double timePerOp = time / opCount * 1e6;
+            System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
+        }
+    }
+
+    public static void main(String[] args) {
+        timeGetLast();
+    }
+
+    public static void timeGetLast() {
+        // TODO: YOUR CODE HERE
+        int[] size = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+        AList<Integer> Ns = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        final int CALL_NUMBER = 10000;
+        for (int i = 0; i < size.length; i++) {
+            Ns.addLast(size[i]);
+            opCounts.addLast(CALL_NUMBER);
+        }
+        AList<Double> times = new AList<>();
+        for (int i = 0; i < size.length; i++) {
+            SLList<Integer> list = new SLList<>();
+            for (int j = 0; j < size[i]; j++) {
+                list.addLast(j);
+            }
+            Stopwatch stopwatch = new Stopwatch();
+            for (int j = 0; j < CALL_NUMBER; j++) {
+                list.getLast();
+            }
+            times.addLast(stopwatch.elapsedTime());
+        }
+        printTimingTable(Ns, times, opCounts);
+    }
+}
